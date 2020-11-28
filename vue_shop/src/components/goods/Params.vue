dce452a2e9df45bdf4d3ca6771a89b38c85a5227
YogLn <15508067205@163.com>
2020-11-28 14:55:19 +0800

    没完成分类参数

---
+++ vue_shop/src/components/goods/Params.vue
@@ -0,0 +1,201 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 警告提示区域 -->
+      <el-alert title="注意：只允许为第三级分类设置相关参数" type="warning" :closable="false" show-icon>
+      </el-alert>
+
+      <!-- 选择商品分类区域 -->
+      <el-row class="cate_opt">
+        <el-col>
+          <span>选择商品分类：</span>
+        </el-col>
+      </el-row>
+      <!-- 选择商品级联选择框 -->
+      <el-cascader v-model="selectedKeys" :options="cateList" :props="{ expandTrigger: 'hover',value: 'cat_pid',
+        label: 'cat_name', children: 'children' }" @change="handleChange"></el-cascader>
+
+      <!-- tag 分页区域 -->
+      <el-tabs v-model="activeName" @tab-click="handleTagClick">
+        <!-- 添加参数区 -->
+        <el-tab-pane label="动态参数" name="many">
+          <el-button size="mini" type="primary" :disabled="isBtnDisabled" @click="addDialogVisible = true">添加参数</el-button>
+          <!-- 动态属性表格 -->
+          <el-table :data="manyTableDate" stripe border>
+            <!-- 展开列 -->
+            <el-table-column type="expand"></el-table-column>
+            <!-- 索引列 -->
+            <el-table-column type="index" label="#"></el-table-column>
+            <el-table-column label="参数名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button type="primary" size="mini" icon="el-icon-edit">编辑</el-button>
+                <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <!-- 静态属性区 -->
+        <el-tab-pane label="静态属性" name="only">
+          <el-button size="mini" type="primary" :disabled="isBtnDisabled" @click="addDialogVisible = true">添加属性</el-button>
+          <!-- 静态属性表格 -->
+          <el-table :data="onlyTableDate" stripe border>
+            <!-- 展开列 -->
+            <el-table-column type="expand"></el-table-column>
+            <!-- 索引列 -->
+            <el-table-column type="index" label="#"></el-table-column>
+            <el-table-column label="属性名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button type="primary" size="mini" icon="el-icon-edit">编辑</el-button>
+                <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+
+    <!-- 添加属性和参数对话框 -->
+    <el-dialog :title="'添加' + titleText" :visible.sync="addDialogVisible" width="50%" @close="addDialogClosed">
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px">
+        <el-form-item :label="titleText" prop="attr_name">
+          <el-input v-model="addForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addParams">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.getCateList()
+  },
+  data() {
+    return {
+      // 获取到的商品列表
+      cateList: [],
+      // 选中的商品id
+      selectedKeys: [],
+      cateProps: {
+        value: 'cat_pid',
+        label: 'cat_name',
+        children: 'children',
+      },
+      // tag 选中的name
+      activeName: 'many',
+      // 动态参数列表
+      manyTableDate: [],
+      // 静态参数列表
+      onlyTableDate: [],
+      addDialogVisible: false,
+      addForm: {
+        attr_name: '',
+      },
+      addFormRules: {
+        attr_name: [
+          { required: true, message: '请输入参数名称', trigger: 'blur' },
+        ],
+      },
+    }
+  },
+  methods: {
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品列表信息失败')
+      }
+      this.cateList = res.data
+    },
+    // 级联选择器改变
+    handleChange() {
+      this.getParamsData()
+    },
+    handleTagClick() {
+      this.getParamsData()
+      console.log(this.activeName)
+    },
+    // 获取参数的列表数据
+    async getParamsData() {
+      // 未选中三级分类
+      if (this.selectedKeys.length !== 3) {
+        return (this.selectedKeys = [])
+      }
+      // 选中三级分类
+      const { data: res } = await this.$http.get(
+        `categories/${this.cateId}/attributes`,
+        {
+          params: { sel: this.activeName },
+        }
+      )
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取参数列表失败')
+      }
+      console.log(res.data)
+      if (this.activeName === 'many') {
+        this.manyTableDate = res.data
+      } else {
+        this.onlyTableDate = res.date
+      }
+    },
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields()
+    },
+    async addParams() {
+      const { data: res } = await this.$http.post(
+        `categories/${this.cateId}/attributes`,
+        {
+          attr_name: this.addForm.attr_name,
+          attr_sel: this.activeName,
+        }
+      )
+      if (res.meta.status !== 201) {
+        return this.$message.error('更新失败')
+      }
+      this.$message.success('更新成功')
+      this.getCateList()
+      this.addDialogVisible = false
+    },
+  },
+  computed: {
+    isBtnDisabled() {
+      if (this.selectedKeys.length !== 3) {
+        return true
+      }
+      return false
+    },
+    cateId() {
+      if (this.selectedKeys.length === 3) {
+        return this.selectedKeys[2]
+      }
+      return null
+    },
+    titleText() {
+      if (this.activeName === 'many') {
+        return '商品参数'
+      } else {
+        return '商品属性'
+      }
+    },
+  },
+}
+</script>
+
+<style>
+.cate_opt {
+  margin: 15px 0;
+}
+</style>
--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import { Button, Form, FormItem, Input, Message, Container, Header, Aside,
          Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card,Row,
          Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox,
-          Tag, Tree, Select, Option, Cascader} from 'element-ui'
+          Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -32,6 +32,9 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
 // 全局挂载message $message 为自定义属性
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
